71420f1466b4e572cca81a65f4cc06bfbf186ea7
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-08 23:04:22 +0100

    Moved all fragments to a single folder

--- app/src/main/java/com/example/leaderboardapp/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/example/leaderboardapp/fragments/RecyclerViewFragment.java
@@ -1,4 +1,4 @@
-package com.example.leaderboardapp;
+package com.example.leaderboardapp.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,29 +10,49 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.leaderboardapp.adapters.LeaderboardRecyclerAdapter;
+import com.example.leaderboardapp.OnSwipeRefreshListener;
+import com.example.leaderboardapp.R;
+import com.example.leaderboardapp.models.TopLearners;
+import com.example.leaderboardapp.models.TopSkillPoints;
 
 import java.util.List;
 
 public class RecyclerViewFragment extends Fragment {
 
     private LeaderboardRecyclerAdapter mAdapter;
+    private OnSwipeRefreshListener mRefreshListener;
+
+    public RecyclerViewFragment() {}
+
+    public RecyclerViewFragment(OnSwipeRefreshListener refreshListener) {
+        mRefreshListener = refreshListener;
+    }
 
     public void setTopLearnersList(List<TopLearners> topLearnersList) {
-        mAdapter.setTopLearners(topLearnersList);
+        if (mAdapter != null)
+            mAdapter.setTopLearners(topLearnersList);
     }
 
     public void setTopSkillPointsList(List<TopSkillPoints> topSkillPointsList) {
-        mAdapter.setTopSkillPoints(topSkillPointsList);
+        if (mAdapter != null)
+            mAdapter.setTopSkillPoints(topSkillPointsList);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setRetainInstance(true);
         return inflater.inflate(R.layout.recycler_view, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        SwipeRefreshLayout refreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        refreshLayout.setOnRefreshListener(() -> mRefreshListener.refresh(refreshLayout));
+
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         mAdapter = new LeaderboardRecyclerAdapter();
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
