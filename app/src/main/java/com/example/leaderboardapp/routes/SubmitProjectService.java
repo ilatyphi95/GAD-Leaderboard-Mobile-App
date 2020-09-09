1e0bb3772e26285fd856735d7d070f2e6a0bda01
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-09 05:06:08 +0100

    Updated the submit route

--- app/src/main/java/com/example/leaderboardapp/routes/SubmitProjectService.java
+++ app/src/main/java/com/example/leaderboardapp/routes/SubmitProjectService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 public interface SubmitProjectService {
 
     @FormUrlEncoded
-    @POST("/submit")
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     Call<Void> submit(
             @Field("entry.1877115667") String firstName,
             @Field("entry.2006916086") String lastName,
