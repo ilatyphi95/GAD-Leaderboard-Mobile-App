f618b3b691471dc0abaca55b6e76c54394fe81aa
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-09 05:13:04 +0100

    Minor update

--- app/src/main/java/com/example/leaderboardapp/models/GADLeaderboard.java
+++ app/src/main/java/com/example/leaderboardapp/models/GADLeaderboard.java
@@ -3,6 +3,6 @@ package com.example.leaderboardapp.models;
 import java.util.List;
 
 public class GADLeaderboard {
-    public List<TopLearners> mTopLearners;
+    public List<TopLearner> mTopLearners;
     public List<TopSkillPoints> mTopSkillPoints;
 }
