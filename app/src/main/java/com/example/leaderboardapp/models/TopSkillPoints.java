f5d9d298db334f17f4bd20ba0f47912dc2cc589c
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-09 05:11:13 +0100

    Updated the model to be a room entity

--- app/src/main/java/com/example/leaderboardapp/models/TopSkillPoints.java
+++ app/src/main/java/com/example/leaderboardapp/models/TopSkillPoints.java
@@ -1,12 +1,59 @@
 package com.example.leaderboardapp.models;
 
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
+
+@Entity
 public class TopSkillPoints {
+    @Ignore
     public static final String URL = "/api/skilliq";
 
+    @PrimaryKey(autoGenerate = true)
+    private int _id;
+
+    @Expose
     private String name;
+    @Expose
     private String score;
+    @Expose
     private String country;
+    @Expose
     private String badgeUrl;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setBadgeUrl(String badgeUrl) {
+        this.badgeUrl = badgeUrl;
+    }
 
     public String getName() {
         return name;
